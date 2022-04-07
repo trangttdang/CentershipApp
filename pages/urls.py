bdf9c8420f5721ce7bd202d63a8f0183773ad118
Emily Kim <ekim010@citymail.cuny.edu>
2022-04-06 21:51:43 -0400

    new changes to templates and pages

---
+++ pages/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView  # new
+
+
+urlpatterns = [
+    path('', views.index, name='site'),
+  
+]
+
+# new
+urlpatterns += [
+    path(r"templates.html", TemplateView.as_view(template_name="pages/templates.html", content_type='text/plain')),
+]
+
