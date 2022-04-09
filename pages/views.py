101f55e08eb8e61ef54504cf4cd1d5f4ac682b06
mpane1 <mpane0104@gmail.com>
2022-04-09 10:56:33 -0400

    changes

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from django.views.generic import TemplateView
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/site.html', {})
+   return render(request, 'pages/site.html')
 
-=======
+
 
 # Create your views here.
->>>>>>> bbdcc41e33e8a206af92713278f7fd54f2edef41
+
+def index2(request):
+   return render(request, 'pages/about.html')
+
+def index3(request):
+   return render(request,'pages/mentors.html')
+
+def index4(request):
+   return render(request,'pages/mentees.html')
+
