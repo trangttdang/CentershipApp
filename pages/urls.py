2b02a81858e72214c886c50c40aab52a5d5e28a8
Emily Kim <ekim010@citymail.cuny.edu>
2022-04-08 21:38:42 -0400

    added 3 sections

--- pages/urls.py
+++ pages/urls.py
@@ -5,9 +5,11 @@ from django.views.generic import TemplateView  # new
 
 urlpatterns = [
    path('', views.index, name='site'),
-  
-]
+   path('about/', views.index2, name='about'),
+   path('mentors/',views.index3, name= 'mentors'),
+   path('mentees/', views.index4, name= 'mentees')
 
+]
 # new
 urlpatterns += [
     path(r"templates.html", TemplateView.as_view(template_name="pages/templates.html", content_type='text/plain')),
