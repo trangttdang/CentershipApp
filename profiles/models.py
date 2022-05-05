bb8b00f258df08e53d5ef351a83f4eec55b94157
Emily Kim <ekim010@citymail.cuny.edu>
2022-05-04 20:17:06 -0400

    slight changes

--- profiles/models.py
+++ profiles/models.py
@@ -2,39 +2,90 @@ from operator import mod
 from django.db import models
 # from django.contrib.auth.models import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
+from django import forms
 # Create your models here.
 
 
 class User(AbstractUser):
-    is_mentor = models.BooleanField(default=False)
-    is_mentee = models.BooleanField(default=False)
-    name = models.CharField(max_length=80)
+    professional_interests = models.TextField(null=True)
+    personal_interests = models.TextField(null=True)
     username = models.CharField(max_length=80, unique= True)
     email = models.CharField(max_length=80)
     password1 = models.CharField(max_length=80)
     password2 = models.CharField(max_length=80)
+    USER_TYPE = (
+        ('mentee','Mentee'),
+        ('mentor','Mentor'),
+    )
+    user_type = models.CharField(max_length=80, null=True, choices=USER_TYPE)
 
 class Mentor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True )
+    education = models.CharField(max_length=80, null=True)
+    professional_experience = models.TextField(null=True)
+    mentee_limit = models.IntegerField(null=True)
+    mentorship_duration = models.DurationField(null=True)
 
+    PROFESSIONAL_INTERESTS = (
+        ('Technical Interviews','Technical Interviews'),
+        ('Web Development','Wed Development'),
+        ('App Development','App Development'),
+        ('Math','Math'),
+        ('Computer Science','Computer Science'),
+        ('Science','Science'),
+        ('Engineering','Engineering'),
+    )
+    PERSONAL_INTERESTS = (
+        ('Painting','Painting'),
+        ('Music','Music'),
+        ('Photogrpahy','Photography'),
+        ('Reading','Reading'),
+        ('Sports','Sports'),
+        ('Cooking','Cooking'),
+    )
+
+    professional_interests = models.CharField(max_length=80, null=True, choices=PROFESSIONAL_INTERESTS)
+    personal_interests = models.CharField(max_length=80, null=True, choices=PERSONAL_INTERESTS)
+
+    
 
 class Mentee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True )
-    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, null= True, default=None)
+    goals = ArrayField(
+        models.TextField(),
+        null=True
+    )
+    # add_goal...would this be defined here or in the view? i feel like it should be defined in the view beacuse how would a function show up on the db? 
+    # delete_goal
+    # find_mentor
 
-# class Persona(models.Model):
-#     # registration = get_user_model()
-#     name = models.CharField(max_length=80)
-#     username = models.CharField(max_length=80)
-#     email = models.CharField(max_length=80)
-#     password1 = models.CharField(max_length=80)
-#     password2 = models.CharField(max_length=80)
+    PROFESSIONAL_INTERESTS = (
+        ('Technical Interviews','Technical Interviews'),
+        ('Web Development','Wed Development'),
+        ('App Development','App Development'),
+        ('Math','Math'),
+        ('Computer Science','Computer Science'),
+        ('Science','Science'),
+        ('Engineering','Engineering'),
+    )
+    PERSONAL_INTERESTS = (
+        ('Painting','Painting'),
+        ('Music','Music'),
+        ('Photogrpahy','Photography'),
+        ('Reading','Reading'),
+        ('Sports','Sports'),
+        ('Cooking','Cooking'),
+    )
+
+    professional_interests = models.CharField(max_length=80, null=True, choices=PROFESSIONAL_INTERESTS)
+    personal_interests = models.CharField(max_length=80, null=True, choices=PERSONAL_INTERESTS)
+
+
 
 # class Mentee(Persona):
 #     # goalList arraylist
-#     # mentee id pk
-#     # mentor id fk
-
 #     # methods:
 #     def goal():
 #         # add or delete a goal
@@ -57,4 +108,3 @@ class Mentee(models.Model):
 
 #     def match():
 #         # launches survey?
-
--- profiles/views.py
+++ profiles/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import  render, redirect
-from .forms import NewMenteeForm, NewMentorForm
+from .forms import MenteeProfileForm, MentorProfileForm, NewUserForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from profiles.models import User
 
 # for register page...so only needs to get?
 def register_request(request):
-	if request.method == "GET":
-		return render(request=request, template_name="register.html")
-	# if request.method == "POST":
-	# 	form = NewUserForm(request.POST)
-	# 	if form.is_valid():
-	# 		user = form.save()
-	# 		login(request, user)
-	# 		messages.success(request, "Registration successful." )
-	# 		return redirect("profiles:homepage")
-	# 	messages.error(request, "Unsuccessful registration. Invalid information.")
-	# form = NewUserForm()
-	# return render (request=request, template_name="register.html", context={"register_form":form})
+	# if request.method == "GET":
+	# 	return render(request=request, template_name="register.html")
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("profiles:login")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="register.html", context={"register_form":form})
 
 def login_request(request):
 	if request.method == "POST":
@@ -29,7 +30,10 @@ def login_request(request):
 			if user is not None:
 				login(request, user)
 				messages.info(request, "You are now logged in as {username}.")
-				return redirect("profiles:homepage")
+				if user.user_type == 'mentor':
+					return redirect("profiles:mentor_profile")
+				else:
+					return redirect("profiles:mentee_profile")	
 			else:
 				messages.error(request,"Invalid username or password.")
 		else:
@@ -38,27 +42,71 @@ def login_request(request):
 	return render(request=request, template_name="login.html", context={"login_form":form})
 
 # for the mentor signup page
-def mentor_request(request):
-	if request.method == "POST":
-		form = NewMentorForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("profiles:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewMentorForm()
-	return render (request=request, template_name="mentor_signup.html", context={"register_form":form})
+# def mentor_request(request):
+# 	if request.method == "POST":
+# 		form = NewMentorForm(request.POST)
+# 		if form.is_valid():
+# 			user = form.save()
+# 			login(request, user)
+# 			messages.success(request, "Registration successful." )
+# 			return redirect("profiles:login")
+# 		messages.error(request, "Unsuccessful registration. Invalid information.")
+# 	form = NewMentorForm()
+# 	return render (request=request, template_name="mentor_signup.html", context={"register_form":form})
 
 # for the mentee signup page
-def mentee_request(request):
+# def mentee_request(request):
+# 	if request.method == "POST":
+# 		form = NewMenteeForm(request.POST)
+# 		if form.is_valid():
+# 			user = form.save()
+# 			login(request, user)
+# 			messages.success(request, "Registration successful." )
+# 			return redirect("profiles:login")
+# 		messages.error(request, "Unsuccessful registration. Invalid information.")
+# 	form = NewMenteeForm()
+# 	return render (request=request, template_name="mentee_signup.html", context={"register_form":form})
+
+
+# for the mentor profile page
+def mentor_profile_request(request):
+	form = MentorProfileForm()
 	if request.method == "POST":
-		form = NewMenteeForm(request.POST)
+		form = MentorProfileForm(request.POST)
 		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("profiles:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewMenteeForm()
-	return render (request=request, template_name="mentee_signup.html", context={"register_form":form})
+			obj = form.save(commit=False) # Return an object without saving to the DB
+			obj.user = User.objects.get(pk = request.user.id) # Add an author field which will contain current user's id
+			obj.save() # Save the final "real form" to the DB
+		else:
+			print("ERROR : Form is invalid")
+		redirect("profiles:mentor_landing")
+	return render (request=request, template_name="update_mentor_profile.html", context={"mentor_profile_form":form})
+
+
+# for the mentee profile page
+def mentee_profile_request(request):
+	form = MenteeProfileForm()
+	if request.method == "POST":
+		form = MenteeProfileForm(request.POST)
+		if form.is_valid():
+			obj = form.save(commit=False) # Return an object without saving to the DB
+			obj.user = User.objects.get(pk = request.user.id) # Add an author field which will contain current user's id
+			obj.save() # Save the final "real form" to the DB
+		else:
+			print("ERROR : Form is invalid")
+		redirect("profiles:mentee_landing")
+	return render (request=request, template_name="update_mentee_profile.html", context={"mentee_profile_form":form})
+
+# # for the mentor landing page; this will also contain GET 
+# def mentor_profile_request(request):
+
+# # for the mentee landing page; this will also contain GET 
+# def mentee_profile_request(request):
+
+def mentee_profile(request):
+	if request.method == "GET":
+		return render(request=request, template_name="mentee_profile.html")
+
+def mentor_profile(request):
+	if request.method == "GET":
+		return render(request=request, template_name="mentor_profile.html")
